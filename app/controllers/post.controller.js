bff3dfd48fcd6b7d0c2700b6eefc26559e6ad2e7
NelWijaya <neldy.wijaya80@gmail.com>
2022-03-19 17:29:38 +0700

    update

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -7,16 +7,99 @@ var cron = require('node-cron')
 
 // Untuk Get data
 exports.findAll = (req, res) => {
-    
-        Post.find()
-            .then((result) => {
-                res.send(result)
-            }).catch((err) => {
-                res.status(500).send({
-                    message: err.message || "Some error while retreieving posts."
+
+    const id = '6231855867c58d847396e2cc'
+
+    Post.findById(id)
+        .then((result) => {
+            res.send(result)
+        }).catch((err) => {
+            res.status(409).send({
+                message: err.message || "Some error while show post."
+            })
+        })
+
+    // terjadwal terpanggil setiap jam 11:59
+    cron.schedule('* * * * *', function() {
+
+
+        const id = '6231855867c58d847396e2cc'
+        let today = new Date()
+        request({
+            url: "https://data.covid19.go.id/public/api/update.json",
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                console.log(error)
+            } else {
+                //Id ini sebagai data utama untuk ditampilkan
+                const id = '6231855867c58d847396e2cc'
+                var data = (body)
+                post = new Post({
+                    jumlah_positif: data.update.penambahan.jumlah_positif,
+                    jumlah_meninggal: data.update.penambahan.jumlah_meninggal,
+                    jumlah_sembuh: data.update.penambahan.jumlah_sembuh,
+                    jumlah_dirawat: data.update.penambahan.jumlah_dirawat,
                 })
-            });
-   
+
+                Post.findById(id)
+                    .then((result) => {
+                        var temp = [result.jumlah_positif, result.jumlah_meninggal, result.jumlah_sembuh, result.jumlah_dirawat];
+
+                        // Panggil data pada API utama
+                        // console.log(result)
+
+                        // Jika data terdapat perubahan
+                        if (temp[0] != post.jumlah_positif || temp[1] != post.jumlah_meninggal || temp[2] != post.jumlah_sembuh || temp[3] != post.jumlah_dirawat) {
+                            // console.log(result)
+
+                            //Simpan data point ke mongoDB
+                            post.save(post)
+                                .then((result) => {
+                                    console.log("Data point baru berhasil diinsert")
+                                        // console.log(result)
+                                }).catch((err) => {
+                                    // console.log("Some error while create post.")
+                                })
+
+                            //Updated data utama
+                            post = {
+                                jumlah_positif: data.update.penambahan.jumlah_positif,
+                                jumlah_meninggal: data.update.penambahan.jumlah_meninggal,
+                                jumlah_sembuh: data.update.penambahan.jumlah_sembuh,
+                                jumlah_dirawat: data.update.penambahan.jumlah_dirawat,
+                            }
+
+                            Post.findByIdAndUpdate(id, post)
+                                .then((result) => {
+                                    if (!result) {
+                                        console.log("Post not found")
+                                    } else {
+                                        console.log("Data berubah, diupdated pada: " + today)
+                                    }
+
+
+                                }).catch((err) => {
+                                    // console.log("Some error while update post.")
+                                })
+                        }
+
+                    }).catch((err) => {
+                        console.log(err)
+                    })
+            }
+        })
+    });
+
+    // Post.find()
+    //     .then((result) => {
+    //         res.send(result)
+    //     }).catch((err) => {
+    //         res.status(500).send({
+    //             message: err.message || "Some error while retreieving posts."
+    //         })
+    //     });
+
 }
 
 
